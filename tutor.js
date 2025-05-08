e49adbfcd8657a1104c3e0f0a93a1eab76d35c8c
Mukhammedali Berektasuly <berektasulymuhammedali@gmail.com>
2025-05-08 23:30:35 +0500

    first commit

---
+++ tutor.js
@@ -0,0 +1,6 @@
+//GLOBALS - NO WINDOW
+//__DIRNAME - path to the current directory
+//--filename - current file name
+//require - function to require modules (common JS)
+//module - info about the current module (file)
+//process - info about env where the code is running
